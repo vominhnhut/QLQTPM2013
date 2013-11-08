c9aa704037d6c062d473327ee4d0eb882cbc5b53
hoaphat <phatnguyen.it.khtn@gmail.com>
2013-11-09 03:23:58 +0700

    Add field LOGICUSER_TOKEN

--- src/com/example/ultils/Constants.java
+++ src/com/example/ultils/Constants.java
@@ -8,6 +8,7 @@ import com.example.Object.DiaDiem;
 
 public class Constants {
 
+	public static String LOGINUSER_TOKEN;
 	public static final String DIADIEM_KEY = "Diadiem";
 
 	public static ArrayList<BinhLuan> createDumdComments() {
@@ -22,29 +23,29 @@ public class Constants {
 		}
 		return bls;
 	}
-	
-	public static ArrayList<DiaDiem> getDumbDDList(){
+
+	public static ArrayList<DiaDiem> getDumbDDList() {
 		ArrayList<DiaDiem> ddList = new ArrayList<DiaDiem>();
-		
-		for(int i=0 ; i<10 ; i++){
+
+		for (int i = 0; i < 10; i++) {
 			DiaDiem dd = new DiaDiem();
-			dd.id=i+"";
-			dd.ten = "Dumb location "+i;
-			dd.diaChi = "Dumb address"+ i;
-			
+			dd.id = i + "";
+			dd.ten = "Dumb location " + i;
+			dd.diaChi = "Dumb address" + i;
+
 			double latitude = 17.385044;
 			double longitude = 78.486671;
-			dd.latitude= latitude+i;
-			dd.longitude = longitude +i;
-			
-			dd.diemDanhGia = i*992;
+			dd.latitude = latitude + i;
+			dd.longitude = longitude + i;
+
+			dd.diemDanhGia = i * 992;
 			dd.isLiked = true;
 			dd.isSaved = true;
 			dd.danhSachBinhLuan = createDumdComments();
-		
+
 			ddList.add(dd);
 		}
 		return ddList;
-		
+
 	}
 }
