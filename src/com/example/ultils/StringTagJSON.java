5f5252ed1cc72a8ff1432ff5d020181bf34bbe05
NAMHOAIVO <vhnam.7@gmail.com>
2013-11-10 18:29:53 +0700

    parse binh luan

--- src/com/example/ultils/StringTagJSON.java
+++ src/com/example/ultils/StringTagJSON.java
@@ -1,6 +1,14 @@
 package com.example.ultils;
-
+/**
+ * 
+ * @author vhnam
+ * Name: Vo Hoai Nam
+ * Email: vhnam.7@gmail.com
+ * Phone: 0985272003
+ * Skype: vhnam.7
+ */
 public class StringTagJSON {
+	//DIA DIEM
 	public static String TAG_MA_DU_LIEU = "ma_du_lieu";
 	public static String TAG_TEN_DICH_VU = "ten_dich_vu";
 	public static String TAG_TEN_DIA_DIEM = "ten_dia_diem";
@@ -13,6 +21,14 @@ public class StringTagJSON {
 	public static String TAG_VI_DO = "vi_do";
 	public static String TAG_CHU_THICH = "chu_thich";
 	public static String TAG_SO_LUOT_THICH = "so_luot_thich";
+	
+	//BINH LUAN
+	public static String TAG_MA_BINH_LUAN = "ma_binh_luan";
+	public static String TAG_TEN_NGUOI_DUNG = "ten_nguoi_dung";
+	public static String TAG_THOI_GIAN_DANG = "thoi_gian_dang";
+	public static String TAG_NOI_DUNG = "noi_dung";
+	
 	public static String TAG_CONTENTString = "content";
+	
 	public static String TAG_SUCCESS = "success";
 }
