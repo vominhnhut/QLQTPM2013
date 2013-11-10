ee1775eb18a90c2adc5b9975d861b0823acc830d
hoaphat <phatnguyen.it.khtn@gmail.com>
2013-11-10 19:31:48 +0700

    Thêm tag
    Signed-off-by: hoaphat <phatnguyen.it.khtn@gmail.com>

--- src/com/example/ultils/StringTagJSON.java
+++ src/com/example/ultils/StringTagJSON.java
@@ -1,14 +1,12 @@
 package com.example.ultils;
+
 /**
  * 
- * @author vhnam
- * Name: Vo Hoai Nam
- * Email: vhnam.7@gmail.com
- * Phone: 0985272003
- * Skype: vhnam.7
+ * @author vhnam Name: Vo Hoai Nam Email: vhnam.7@gmail.com Phone: 0985272003
+ *         Skype: vhnam.7
  */
 public class StringTagJSON {
-	//DIA DIEM
+	// DIA DIEM
 	public static String TAG_MA_DU_LIEU = "ma_du_lieu";
 	public static String TAG_TEN_DICH_VU = "ten_dich_vu";
 	public static String TAG_TEN_DIA_DIEM = "ten_dia_diem";
@@ -21,14 +19,20 @@ public class StringTagJSON {
 	public static String TAG_VI_DO = "vi_do";
 	public static String TAG_CHU_THICH = "chu_thich";
 	public static String TAG_SO_LUOT_THICH = "so_luot_thich";
-	
-	//BINH LUAN
+
+	// BINH LUAN
 	public static String TAG_MA_BINH_LUAN = "ma_binh_luan";
 	public static String TAG_TEN_NGUOI_DUNG = "ten_nguoi_dung";
 	public static String TAG_THOI_GIAN_DANG = "thoi_gian_dang";
 	public static String TAG_NOI_DUNG = "noi_dung";
-	
 	public static String TAG_CONTENTString = "content";
-	
 	public static String TAG_SUCCESS = "success";
+	
+	//TAIKHOAN
+	public static String TAG_TEN_TAI_KHOAN = "ten_tai_khoan";
+	public static String TAG_MAT_KHAU = "mat_khau";
+	public static String TAG_HO_VA_TEN = "ho_va_ten";
+	public static String TAG_EMAIL = "email";
+	public static String TAG_NGAY_SINH = "ngay_sinh";
+	
 }
